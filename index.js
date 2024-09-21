a83fd99012741ec042b4bbccc00e682192d97625
Guinn <eguinjonathan08@gmail.com>
2024-09-21 17:06:08 +0700

    added font and animation

---
+++ index.js
@@ -0,0 +1,10 @@
+function updateClock(){
+    const now = new Date();
+    const hours = now.getHours().toString().padStart(2, "0");
+    const minutes = now.getMinutes().toString().padStart(2, "0");
+    const seconds = now.getSeconds().toString().padStart(2, "0");
+    const time = `${hours}:${minutes}:${seconds}`;
+    document.getElementById("clock").innerHTML = time;
+}
+
+setInterval(updateClock, 1000)
